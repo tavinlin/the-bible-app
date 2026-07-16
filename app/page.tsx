e873550f1957f7bc6c85a74d41faf629856a915b
Tavin Lin <tavinlin1519@gmail.com>
2026-07-15 23:23:14 -0400

    Created the home of the app - Disabled devIndicator - Updated README - Updated favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,27 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const data = await fetch('https://bible-api.com/data/web/random')
+  const random = await data.json()
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <section className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 md:pt-32 md:pb-28 flex flex-col justify-center min-h-[calc(100vh-4rem)]">
+        <div className="text-center">
+          <p className="text-indigo-400 font-mono text-lg sm:text-1xl mb-3 tracking-wide">Welcome to</p>
+              <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-slate-100 tracking-tight mb-4">
+                  The Bible App
+              </h1>
+              <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold text-slate-400 tracking-tight mb-6">
+                  {random.random_verse.book} {random.random_verse.chapter}:{random.random_verse.verse}
+              </h2>
+              <p className="text-slate-400 text-base sm:text-1xl md:text-2xl mb-10">
+                  {random.random_verse.text}
+              </p>
+              <a href="#" className="inline-flex items-center justify-center px-6 py-3 border border-indigo-500 text-base font-medium rounded-md text-indigo-400 bg-transparent hover:bg-indigo-500/10 transition-colors duration-200">
+                  Books in English
+              </a>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    </section>
   );
 }
